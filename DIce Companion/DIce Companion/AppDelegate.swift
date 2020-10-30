499bc3a0145cd508c29b5d3551ec357897bedcde
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2020-10-30 14:33:12 -0400

    Initial Xcode Project Creation

---
+++ DIce Companion/DIce Companion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DIce Companion
+//
+//  Created by Bradley GIlmore on 10/30/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DIce Companion/DIce Companion/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DIce Companion
+//
+//  Created by Bradley GIlmore on 10/30/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
